8ab531cc5df2aef8796f2edc455d13bd07f2b11b
Isaias Junior Anacleto Amorim <isaias.amorim@hypera.com.br>
2024-06-21 10:36:09 -0300

    Subir novo projeto calculadora

---
+++ Calculadora/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Calculadora;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Primeiro valor: ");
+        float valor1 = float.Parse(Console.ReadLine());
+
+        Console.WriteLine(valor1);
+
+
+    }
+}
